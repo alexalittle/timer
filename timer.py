b8b218089111f2765c38fc0271d52abf5033bb02
Alexa Little <alittle@7000.org>
2020-10-17 18:19:10 -0400

    Restructured as OOP
    Restructured the app as an "App" class with instance methods
    
    Added a toy "binding.py" example to troubleshoot before restructuring the main app

--- timer.py
+++ timer.py
@@ -1,18 +1,68 @@
 import tkinter as tk
 
-class Timer:
+class App:
+	def __init__(self, master):
+		frame = tk.Frame(master)
+		frame.pack()
 
-	def __init__(self, start_len):
-		self.start_len = Timer.min_to_sec(start_len)
-		self.current_len = Timer.min_to_sec(start_len)
-		self.running = None
+		# set attributes for start time, current time
+		self.start_time = 0
+		self.current_time = 0
 
-	def __repr__(self):
-		minutes = Timer.sec_to_min(self.start_len)
-		return f"A {minutes} timer"
+		# set up labels
+		self.title = tk.Label(frame, text = "Timer")
+		self.timer_display = tk.Label(frame, text = "0:00")
 
-	def reset(self):
-		self.current_len = self.start_len
+		# create start, stop, reset buttons
+		self.start_btn = tk.Button(frame, text="Start", command = self.start_timer)
+		self.stop_btn = tk.Button(frame, text="Stop", command = self.stop_timer)
+		self.reset_btn = tk.Button(frame, text="Reset", command = self.reset_timer)
+
+		# create section for adding minutes
+		self.minutes_label = tk.Label(frame, text = "Add Minutes")
+		self.minutes_plus_btn = tk.Button(frame, text = "+", command = self.add_mins)
+		self.minutes_minus_btn = tk.Button(frame, text = "-", command = self.remove_mins)
+
+		# pack elements into the frame
+		self.title.pack()
+		self.timer_display.pack()
+
+		self.start_btn.pack()
+		self.stop_btn.pack()
+		self.reset_btn.pack()
+
+		self.minutes_label.pack()
+		self.minutes_plus_btn.pack()
+		self.minutes_minus_btn.pack()
+
+	def start_timer(self):
+		"""timer1 = Timer(timer_display["text"])
+		return run_timer(timer1)"""
+		pass
+
+	def run_timer(my_timer):
+		"""
+		timer_display["text"] = Timer.sec_to_min(my_timer.current_len)
+		if my_timer.current_len > 0:
+			my_timer.current_len -= 1
+			my_timer.running = window.after(1000, lambda: run_timer(my_timer))
+		else:
+			timer_display["text"] = "DONE"
+			window.bell()
+		"""
+		pass
+
+	def stop_timer(self):
+		pass
+
+	def reset_timer(self):
+		pass
+
+	def add_mins(self):
+		pass
+
+	def remove_mins(self):
+		pass
 
 	@classmethod
 	def sec_to_min(cls, seconds):
@@ -28,53 +78,9 @@ class Timer:
 	def min_to_sec(cls, minutes):
 		""" given input in 0:00 format, returns equivalent value in seconds """
 		vals = minutes.split(":")
-		return (int(vals[0]) * 60) + int(vals[1])
-		
+		return (int(vals[0]) * 60) + int(vals[1])		
 
-window = tk.Tk()
+root = tk.Tk()
+app = App(root)
 
-my_timer = Timer("0:00")
-
-title = tk.Label( text = "Timer")
-title.pack()
-
-timer_display = tk.Label(text = Timer.sec_to_min(my_timer.current_len))
-timer_display.pack()
-
-minutes_label = tk.Label(text = "Add Minutes")
-minutes_plus_btn = tk.Button(text = "+")
-minutes_minus_btn = tk.Button(text = "-")
-
-minutes_label.pack()
-minutes_plus_btn.pack()
-minutes_minus_btn.pack()
-
-timer_start_btn = tk.Button(text = "Start")
-
-def start_timer(event):
-	timer1 = Timer(timer_display["text"])
-	return run_timer(timer1)
-
-def run_timer(my_timer):
-	timer_display["text"] = Timer.sec_to_min(my_timer.current_len)
-	if my_timer.current_len > 0:
-		my_timer.current_len -= 1
-		my_timer.running = window.after(1000, lambda: run_timer(my_timer))
-	else:
-		timer_display["text"] = "DONE"
-		window.bell()
-
-timer_start_btn.pack()
-timer_start_btn.bind("<Button-1>", start_timer)
-
-def stop_timer(event):
-	pass
-
-timer_stop = tk.Button(
-	text = "Stop",
-	foreground = "black")
-
-timer_stop.pack()
-timer_stop.bind("<Button-1>", stop_timer)
-
-window.mainloop()
+root.mainloop()
