ae29bb9b4fc11529200947d54cd5a3ab6be1e2cd
Alexa Little <alittle@7000.org>
2020-10-17 20:55:04 -0400

    Cosmetic upgrades
    Added frames to better organize the buttons.
    
    Enlarged font size to make timer more visible.
    
    Changed settings for app window (title, window size).

--- timer.py
+++ timer.py
@@ -2,8 +2,6 @@ import tkinter as tk
 
 class App:
 	def __init__(self, master):
-		frame = tk.Frame(master)
-		frame.pack()
 
 		# set attributes for start time, current time
 		self.start_time = 0
@@ -13,42 +11,53 @@ class App:
 		# stores the current call to tkinter's "after" module
 		self.callback = None
 
-		# set up labels
-		self.title = tk.Label(frame, text = "Timer")
-		self.timer_display = tk.Label(frame, text = "0:00")
+		# create section for main display
+		title_frame = tk.Frame(master)
+		title_frame.pack(side = tk.TOP)
 
-		# create start, stop, reset buttons
-		self.start_btn = tk.Button(frame, text="Start", command = self.start_timer)
-		self.stop_btn = tk.Button(frame, text="Stop", command = self.stop_timer)
-		self.reset_btn = tk.Button(frame, text="Reset", command = self.reset_timer)
-		self.clear_btn = tk.Button(frame, text="Clear", command = self.clear_timer)
-
-		# create section for adding minutes
-		self.minutes_label = tk.Label(frame, text = "Add Minutes")
-		self.minutes_plus_btn = tk.Button(frame, text = "+", command = lambda: self.change_time(60))
-		self.minutes_minus_btn = tk.Button(frame, text = "-", command = lambda: self.change_time(-60))
-
-		#create section for adding seconds
-		self.seconds_label = tk.Label(frame, text = "Add Seconds")
-		self.seconds_plus_btn = tk.Button(frame, text = "+", command = lambda: self.change_time(1))
-		self.seconds_minus_btn = tk.Button(frame, text = "-", command = lambda: self.change_time(-1))
-
-		# pack elements into the frame
+		# set up main display
+		self.title = tk.Label(title_frame, text = "Timer", font=(None, 16))
 		self.title.pack()
+		self.timer_display = tk.Label(title_frame, text = "0:00", font=(None, 24))
 		self.timer_display.pack()
 
-		self.start_btn.pack()
-		self.stop_btn.pack()
-		self.reset_btn.pack()
-		self.clear_btn.pack()
+		# create section for control buttons
+		control_frame = tk.Frame(master)
+		control_frame.pack(side = tk.TOP)
 
-		self.minutes_label.pack()
-		self.minutes_plus_btn.pack()
-		self.minutes_minus_btn.pack()
+		# create start, stop, reset, clear buttons
+		self.start_btn = tk.Button(control_frame, text="Start", command = self.start_timer)
+		self.start_btn.pack(side = tk.LEFT)
+		self.stop_btn = tk.Button(control_frame, text="Stop", command = self.stop_timer)
+		self.stop_btn.pack(side = tk.LEFT)
+		self.reset_btn = tk.Button(control_frame, text="Reset", command = self.reset_timer)
+		self.reset_btn.pack(side = tk.LEFT)
+		self.clear_btn = tk.Button(control_frame, text="Clear", command = self.clear_timer)
+		self.clear_btn.pack(side = tk.LEFT)
 
-		self.seconds_label.pack()
-		self.seconds_plus_btn.pack()
-		self.seconds_minus_btn.pack()
+		# create section for adding minutes
+		minutes_frame = tk.Frame(master)
+		minutes_frame.pack()
+
+		# create buttons for adding minutes
+		self.minutes_label = tk.Label(minutes_frame, text = "Add Minutes")
+		self.minutes_label.pack(side = tk.TOP)
+		self.minutes_minus_btn = tk.Button(minutes_frame, text = "-", command = lambda: self.change_time(-60))
+		self.minutes_minus_btn.pack(side = tk.LEFT)
+		self.minutes_plus_btn = tk.Button(minutes_frame, text = "+", command = lambda: self.change_time(60))
+		self.minutes_plus_btn.pack(side = tk.RIGHT)
+
+		# create section for adding seconds
+		seconds_frame = tk.Frame(master)
+		seconds_frame.pack()
+
+		#create section for adding seconds
+		self.seconds_label = tk.Label(seconds_frame, text = "Add Seconds")
+		self.seconds_label.pack(side = tk.TOP)
+		self.seconds_minus_btn = tk.Button(seconds_frame, text = "-", command = lambda: self.change_time(-1))
+		self.seconds_minus_btn.pack(side = tk.LEFT)
+		self.seconds_plus_btn = tk.Button(seconds_frame, text = "+", command = lambda: self.change_time(1))
+		self.seconds_plus_btn.pack(side = tk.RIGHT)
 
 	def update_display(self):
 		""" Updates the timer on the app display."""
@@ -56,12 +65,13 @@ class App:
 
 	def start_timer(self):
 		""" Starts the timer."""
-		self.current_time -= 1
-		self.update_display()
-		if self.current_time > 0:
-			self.callback = root.after(1000, lambda: self.start_timer())
-		else:
-			root.bell()
+		if self.start_time > 0:
+			self.current_time -= 1
+			self.update_display()
+			if self.current_time > 0:
+				self.callback = root.after(1000, lambda: self.start_timer())
+			else:
+				root.bell()
 
 	def stop_timer(self):
 		""" Stops the timer."""
@@ -82,12 +92,11 @@ class App:
 		self.update_display()
 
 	def change_time(self, num):
-		""" Given an input in seconds, adds that value to the current time. Current time cannot be < 0."""
-		self.current_time += num
-		self.start_time += num
-		if self.current_time < 0:
-			self.current_time = 0
-		self.update_display()
+		""" Given an input in seconds, adds that value to the current time. Prevents negative time values."""
+		if self.current_time + num >= 0:
+			self.current_time += num
+			self.start_time += num
+			self.update_display()
 
 	def sec_to_min(self, seconds):
 		""" Given input in seconds, returns equivalent value in minutes (0:00 string)."""
@@ -99,6 +108,8 @@ class App:
 		return str(calculation[0]) + ":" + output
 
 root = tk.Tk()
+root.geometry("300x200")
+root.title("Timer")
 app = App(root)
 
 root.mainloop()
