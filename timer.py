ace8958c09af919ef14bd6d8def23bd5d2a46bcc
Alexa Little <alittle@7000.org>
2020-10-17 20:12:25 -0400

    Working draft
    Implemented all functions. Noticed a bug with reset_timer, will fix in next commit.

--- timer.py
+++ timer.py
@@ -9,6 +9,10 @@ class App:
 		self.start_time = 0
 		self.current_time = 0
 
+		# create variable to be used by start & stop timer functions
+		# stores the current call to tkinter's "after" module
+		self.callback = None
+
 		# set up labels
 		self.title = tk.Label(frame, text = "Timer")
 		self.timer_display = tk.Label(frame, text = "0:00")
@@ -17,6 +21,7 @@ class App:
 		self.start_btn = tk.Button(frame, text="Start", command = self.start_timer)
 		self.stop_btn = tk.Button(frame, text="Stop", command = self.stop_timer)
 		self.reset_btn = tk.Button(frame, text="Reset", command = self.reset_timer)
+		self.clear_btn = tk.Button(frame, text="Clear", command = self.clear_timer)
 
 		# create section for adding minutes
 		self.minutes_label = tk.Label(frame, text = "Add Minutes")
@@ -35,6 +40,7 @@ class App:
 		self.start_btn.pack()
 		self.stop_btn.pack()
 		self.reset_btn.pack()
+		self.clear_btn.pack()
 
 		self.minutes_label.pack()
 		self.minutes_plus_btn.pack()
@@ -44,38 +50,41 @@ class App:
 		self.seconds_plus_btn.pack()
 		self.seconds_minus_btn.pack()
 
-	def start_timer(self):
-		"""timer1 = Timer(timer_display["text"])
-		return run_timer(timer1)"""
-		pass
-
-	def run_timer(my_timer):
-		"""
-		timer_display["text"] = Timer.sec_to_min(my_timer.current_len)
-		if my_timer.current_len > 0:
-			my_timer.current_len -= 1
-			my_timer.running = window.after(1000, lambda: run_timer(my_timer))
-		else:
-			timer_display["text"] = "DONE"
-			window.bell()
-		"""
-		pass
-
-	def stop_timer(self):
-		pass
-
-	def reset_timer(self):
-		self.current_time = self.start_time
-		self.timer_display["text"] = self.sec_to_min(self.start_time)
-
-	def change_time(self, num):
-		self.current_time += num
-		if self.current_time < 0:
-			self.current_time = 0
+	def update_display(self):
 		self.timer_display["text"] = self.sec_to_min(self.current_time)
 
+	def start_timer(self):
+		self.current_time -= 1
+		self.update_display()
+		if self.current_time > 0:
+			self.callback = root.after(1000, lambda: self.start_timer())
+		else:
+			root.bell()
+
+	def stop_timer(self):
+		if self.callback:
+			root.after_cancel(self.callback)
+
+	def reset_timer(self):
+		# this is a little buggy still
+		self.current_time = self.start_time
+		self.update_display()
+
+	def clear_timer(self):
+		self.current_time = 0
+		self.start_time = 0
+		self.update_display()
+
+	def change_time(self, num):
+		""" Given an input in seconds, adds that value to the current time. Current time cannot be < 0."""
+		self.current_time += num
+		self.start_time += num
+		if self.current_time < 0:
+			self.current_time = 0
+		self.update_display()
+
 	def sec_to_min(self, seconds):
-		""" given input in seconds, returns equivalent value in minutes (0:00 string) """
+		""" Given input in seconds, returns equivalent value in minutes (0:00 string). """
 		calculation = divmod(seconds, 60)
 		if calculation[1] < 10:
 			output = "0" + str(calculation[1])
@@ -84,7 +93,7 @@ class App:
 		return str(calculation[0]) + ":" + output
 
 	def min_to_sec(self, minutes):
-		""" given input in 0:00 format, returns equivalent value in seconds """
+		""" Given input in 0:00 format, returns equivalent value in seconds (int). """
 		vals = minutes.split(":")
 		return (int(vals[0]) * 60) + int(vals[1])		
 
